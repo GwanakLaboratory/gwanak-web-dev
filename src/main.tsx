5024fed790d0740b597a8973f3cab1c052eddf51
최동준 <coine00.choi@gmail.com>
2025-04-08 16:40:08 +0900

    feat: jotai store 생성

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { Provider as JotaiProvider } from 'jotai';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { jotaiStore } from './store/auth.ts';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <JotaiProvider>
+    <JotaiProvider store={jotaiStore}>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,3 +1,5 @@
 import { atom } from 'jotai';
+import { createStore } from 'jotai';
 
+export const jotaiStore = createStore();
 export const accessTokenAtom = atom<string | null>(null);
