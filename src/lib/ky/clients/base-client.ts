9357be746e8b9f5265a5567a106cce351a193fcf
최동준 <coine00.choi@gmail.com>
2025-04-08 12:48:13 +0900

    feat: ky에서 메타 데이터 처리 방식
    *.query.ts에서 meta 데이터를 주입받아서 비즈니스 데이터와 메타 데이터가 분리될 수 있도록 한다.

--- src/lib/ky/clients/base-client.ts
+++ src/lib/ky/clients/base-client.ts
@@ -2,12 +2,19 @@ import type { KyInstance, ResponsePromise } from 'ky';
 
 import { TargetEndpoint } from '../types';
 
+type RequestMeta = {
+  accessToken?: string;
+  locale?: string;
+  deviceId?: string;
+  platform?: 'web' | 'ios' | 'android';
+};
+
 export class BaseClient {
   constructor(private readonly instance: KyInstance) {}
 
-  async json<T>(endpoint: TargetEndpoint): Promise<T> {
+  async json<T>(endpoint: TargetEndpoint, meta?: RequestMeta): Promise<T> {
     try {
-      const response = await this.getResponse<T>(endpoint);
+      const response = await this.getResponse<T>(endpoint, meta);
       return response.json();
     } catch (e) {
       throw new Error(`API_ERROR: ${e}`);
@@ -17,9 +24,10 @@ export class BaseClient {
   async statusCode(
     endpoint: TargetEndpoint,
     statusCodes: [number],
+    meta?: RequestMeta,
   ): Promise<boolean> {
     try {
-      const response = await this.getResponse(endpoint);
+      const response = await this.getResponse(endpoint, meta);
       return statusCodes.includes(response.status);
     } catch (e) {
       throw new Error(`API_ERROR: ${e}`);
@@ -28,6 +36,7 @@ export class BaseClient {
 
   private async getResponse<T>(
     endpoint: TargetEndpoint,
+    meta?: RequestMeta,
   ): Promise<ResponsePromise<T>> {
     let path = endpoint.path;
 
@@ -39,10 +48,26 @@ export class BaseClient {
       );
     }
 
+    const headers = {
+      ...endpoint.config.headers,
+      ...(meta?.accessToken && {
+        Authorization: `Bearer ${meta.accessToken}`,
+      }),
+      ...(meta?.locale && {
+        'X-Locale': meta.locale,
+      }),
+      ...(meta?.deviceId && {
+        'X-Device-Id': meta.deviceId,
+      }),
+      ...(meta?.platform && {
+        'X-Platform': meta.platform,
+      }),
+    };
+
     return this.instance(path, {
       method: endpoint.config.method,
       json: endpoint.config.data,
-      headers: endpoint.config.headers,
+      headers: headers,
       searchParams: endpoint.config.params,
       retry: endpoint.config.retry ?? 3,
       timeout: endpoint.config.timeout ?? 3000,
