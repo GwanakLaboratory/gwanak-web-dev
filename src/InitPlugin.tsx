72e2108f62c355a4e24f72a3ca1d9976858bed6c
최동준 <coine00.choi@gmail.com>
2025-04-11 13:11:15 +0900

    feat: 로그아웃 이벤트 리스너 설정
    토큰 만료로 인한 403 에러 발생 시 강제로 로그아웃을 진행합니다.

--- src/InitPlugin.tsx
+++ src/InitPlugin.tsx
@@ -1,14 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useRefreshTokenMutation } from './apis';
+import { useSetAtom } from 'jotai';
+import { accessTokenAtom } from './store/auth';
+import { useNavigate } from 'react-router-dom';
 
 const InitPlugin = ({ children }: { children: React.ReactNode }) => {
   const [isReady, setIsReady] = useState(false);
   const refreshMutation = useRefreshTokenMutation();
+  const setAccessToken = useSetAtom(accessTokenAtom);
+  const navigate = useNavigate();
 
   useEffect(() => {
     refreshMutation.mutateAsync().finally(() => {
       setIsReady(true);
     });
+  }, []);
+
+  useEffect(() => {
+    const addLogOutEventListner = () => {
+      window.addEventListener('FORCE_LOGOUT', () => {
+        setAccessToken(null);
+        navigate('/home', { replace: true });
+      });
+    };
+    return addLogOutEventListner;
   }, []);
 
   if (!isReady) return <p>loading...</p>;
