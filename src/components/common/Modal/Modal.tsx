cc206e6cf8dd8e3687b44ab83e25940663e813e1
최동준 <coine00.choi@gmail.com>
2025-04-16 00:28:55 +0900

    feat: portal로 modal 컴포넌트 구현

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,9 +1,28 @@
 import ModalPortal from './ModalPortal';
+import { S } from './style';
 
-const Modal = () => {
+interface IModalProps {
+  isOpen: boolean;
+  onLogout: () => void;
+  onClose: () => void;
+}
+
+const Modal = ({ isOpen, onLogout, onClose }: IModalProps) => {
+  if (!isOpen) return null;
+
   return (
     <ModalPortal>
-      <p>modal</p>
+      <S.ModalOuterStyle>
+        <S.ModalInnerStyle>
+          <S.ModalBodyStyle>로그아웃 하시겠습니까?</S.ModalBodyStyle>
+          <S.ModalButtonConatiner>
+            <S.ModalButtonStyle style={{ color: 'red' }} onClick={onLogout}>
+              로그아웃
+            </S.ModalButtonStyle>
+            <S.ModalButtonStyle onClick={onClose}>취소</S.ModalButtonStyle>
+          </S.ModalButtonConatiner>
+        </S.ModalInnerStyle>
+      </S.ModalOuterStyle>
     </ModalPortal>
   );
 };
