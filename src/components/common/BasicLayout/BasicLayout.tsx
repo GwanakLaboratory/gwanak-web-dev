257a1c667b0104ce6e2b868adb3987894336f0d5
최동준 <coine00.choi@gmail.com>
2025-04-16 00:30:20 +0900

    feat: 기본 레이아웃 설정 및 네비게이션 랜딩 데이터 설정

--- src/components/common/BasicLayout/BasicLayout.tsx
+++ src/components/common/BasicLayout/BasicLayout.tsx
@@ -1,6 +1,37 @@
 import { Outlet } from 'react-router-dom';
+import Navigation from '../Navigation';
+import { S } from './style';
+
+const navList = [
+  { name: '회사소개', link: 'about' },
+  {
+    name: '기술과 서비스',
+    link: '#',
+    nested: [
+      { name: '서비스 소개', link: 'service' },
+      { name: '|', link: '#' },
+      { name: '테크 / 특허', link: 'tech' },
+    ],
+  },
+  {
+    name: '포트폴리오',
+    link: '#',
+    nested: [
+      { name: '내역', link: 'auth/portfolios' },
+      { name: '|', link: '#' },
+      { name: '만들기', link: 'auth/portfolios/create' },
+    ],
+  },
+];
 
 const BasicLayout = () => {
-  return <Outlet />;
+  return (
+    <S.OuterStyle>
+      <S.InnerStyle>
+        <Navigation navList={navList} />
+        <Outlet />
+      </S.InnerStyle>
+    </S.OuterStyle>
+  );
 };
 export default BasicLayout;
---
+++ src/components/common/BasicLayout/style.ts
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import { BREAKPOINT } from '../../../lib/constants';
+
+export const S = {
+  OuterStyle: styled.div`
+    position: relative;
+    top: 0px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+  `,
+  InnerStyle: styled.div`
+    postion: relative;
+    top: 0px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+
+    // @media (max-width: ${BREAKPOINT.tablet}) {
+    //   min-height: calc(100vh - 300px);
+    // }
+  `,
+};
