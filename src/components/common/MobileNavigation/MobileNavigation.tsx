3f755157fef8cbe469092bc8817d123570423f8d
최동준 <coine00.choi@gmail.com>
2025-04-16 00:30:47 +0900

    feat: mobile 대응 네비게이션 개발

---
+++ src/components/common/MobileNavigation/MobileNavigation.tsx
@@ -0,0 +1,135 @@
+import { Dispatch, useState } from 'react';
+import { NavigationItemProps } from '../Navigation/Navigation';
+import { SetStateAction, useAtomValue } from 'jotai';
+import { accessTokenAtom } from '../../../store/auth';
+import { S } from './style';
+import { Link, NavLink } from 'react-router-dom';
+import Text from '../Text';
+import Modal from '../Modal';
+import { useSignOutMutation } from '../../../apis';
+
+type MobileNavigationProps = {
+  navList: NavigationItemProps[];
+  isMobileMenuOpen: boolean;
+  setIsMobileMenuOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const MobileNavigation = ({
+  navList,
+  isMobileMenuOpen,
+  setIsMobileMenuOpen,
+}: MobileNavigationProps) => {
+  const [clickedItems, setClickedItems] = useState<boolean[]>(
+    new Array(navList.length).fill(false),
+  );
+  const accessToken = useAtomValue(accessTokenAtom);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const logoutMutation = useSignOutMutation();
+
+  const renderNavLink = (
+    to: string,
+    label: string,
+    onClick?: (e: React.MouseEvent) => void,
+  ) => (
+    <NavLink
+      end
+      to={to}
+      className={({ isActive, isPending }) =>
+        isActive ? 'active' : isPending ? 'pending' : ''
+      }
+      onClick={(e: React.MouseEvent) => {
+        e.stopPropagation();
+        onClick?.(e);
+      }}
+    >
+      <Text color="White" typograph="lg_regular">
+        {label}
+      </Text>
+    </NavLink>
+  );
+
+  const renderMainItem = (item: NavigationItemProps, idx: number) => {
+    const isMainItemClicked = clickedItems[idx];
+    return (
+      <S.NavItem key={idx} onClick={() => toggleItemClick(idx)}>
+        <Link to="#">
+          <Text color="White" typograph="lg_regular">
+            {item.name}
+          </Text>
+        </Link>
+        {isMainItemClicked && item.nested && (
+          <S.NestedItemsContainer>
+            {item.nested.map(
+              (nested) =>
+                nested.link !== '#' && (
+                  <S.NestedItem key={nested.name}>
+                    {renderNavLink('/' + nested.link, nested.name, () =>
+                      setIsMobileMenuOpen(false),
+                    )}
+                  </S.NestedItem>
+                ),
+            )}
+          </S.NestedItemsContainer>
+        )}
+      </S.NavItem>
+    );
+  };
+
+  const renderAuthItem = () => {
+    return accessToken === null || accessToken === '' ? (
+      <S.NavItem key="login">{renderNavLink('/login', '로그인')}</S.NavItem>
+    ) : (
+      <S.NavItem key="logout" onClick={() => setIsModalOpen(true)}>
+        <Text
+          color="White"
+          typograph="lg_regular"
+          style={{ cursor: 'pointer' }}
+        >
+          로그아웃
+        </Text>
+      </S.NavItem>
+    );
+  };
+  const toggleItemClick = (index: number) => {
+    const updatedClickedItems = [...clickedItems];
+    updatedClickedItems[index] = !updatedClickedItems[index];
+    setClickedItems(updatedClickedItems);
+  };
+
+  return (
+    <S.MobileMenuContainer isOpen={isMobileMenuOpen} className="layout-padding">
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onLogout={() => {
+            logoutMutation.mutate();
+            setIsModalOpen(false);
+            setIsMobileMenuOpen(false);
+          }}
+          onClose={() => setIsModalOpen(false)}
+        ></Modal>
+      )}
+
+      <S.Overlay
+        isOpen={isMobileMenuOpen}
+        onClick={() => setIsMobileMenuOpen(false)}
+      />
+      <S.MobileMenuList>
+        {navList.map((item, idx) =>
+          item.nested ? (
+            renderMainItem(item, idx)
+          ) : (
+            <S.NavItem key={idx}>
+              {renderNavLink(`/${item.link}`, item.name, () =>
+                setIsMobileMenuOpen(false),
+              )}
+            </S.NavItem>
+          ),
+        )}
+        {renderAuthItem()}
+      </S.MobileMenuList>
+    </S.MobileMenuContainer>
+  );
+};
+
+export default MobileNavigation;
---
+++ src/components/common/MobileNavigation/style.ts
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+import { BREAKPOINT } from '../../../lib/constants';
+
+export const S = {
+  MobileMenuContainer: styled.div<{ isOpen: boolean }>`
+    position: fixed;
+    top: 0;
+    right: ${(props) => (props.isOpen ? '0' : '-100%')};
+    width: 70%;
+    height: 100%;
+    background-color: #151515;
+    transition: right 0.3s ease-in-out;
+    z-index: 1;
+    overflow: hidden;
+    opacity: 0.8;
+    padding-top: 20px;
+
+    @media (min-width: ${BREAKPOINT.tablet}) {
+      display: none;
+    }
+  `,
+  MobileMenuList: styled.ul`
+    width: 100%;
+    list-style-type: none;
+    padding: 0px;
+  `,
+  NavItem: styled.li`
+    padding: 10px 20px;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    a {
+      text-decoration: none;
+    }
+  `,
+  NestedItemsContainer: styled.ul`
+    padding: 10px 30px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    list-style-type: none;
+    background-color: inherit;
+  `,
+  NestedItem: styled.li`
+    padding-right: 20px;
+  `,
+  Overlay: styled.div<{ isOpen: boolean }>`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: ${(props) => (props.isOpen ? '100%' : '0')};
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5); // 반투명한 배경 추가
+
+    transition: width 0.3s ease-in-out;
+    pointer-events: ${(props) => (props.isOpen ? 'auto' : 'none')};
+  `,
+};
