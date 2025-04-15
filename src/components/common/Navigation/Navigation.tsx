9e030d6e7b7f463c8c4a43fc49950b5b8dc49152
최동준 <coine00.choi@gmail.com>
2025-04-16 00:31:17 +0900

    feat: 기본 네비게이션 바 개발

---
+++ src/components/common/Navigation/Navigation.tsx
@@ -0,0 +1,137 @@
+import { Link, NavLink } from 'react-router-dom';
+import { S } from './style';
+import { useState } from 'react';
+import { useAtomValue } from 'jotai';
+import { accessTokenAtom } from '../../../store/auth';
+import LogoWhiteSVG from '../../../lib/assets/images/logo_white.svg';
+import Modal from '../Modal';
+import Text from '../Text';
+import { useSignOutMutation } from '../../../apis';
+import MobileNavigation from '../MobileNavigation';
+export type NavigationItemProps = {
+  name: string;
+  link: string;
+  nested?: NavigationItemProps[];
+};
+
+const Navigation = ({ navList }: { navList: NavigationItemProps[] }) => {
+  const accessToken = useAtomValue(accessTokenAtom);
+  const [hoveredItem, setHoveredItem] = useState<number | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const logoutMutation = useSignOutMutation();
+  const renderNavLink = (to: string, label: string) => (
+    <NavLink
+      end
+      to={to}
+      className={({ isActive, isPending }) =>
+        isActive ? 'active' : isPending ? 'pending' : ''
+      }
+    >
+      <Text color="White" typograph="lg_regular">
+        {label}
+      </Text>
+    </NavLink>
+  );
+
+  const renderNavItems = () =>
+    navList.map((item, idx) => {
+      const isHovered = hoveredItem === idx;
+
+      if (!item.nested) {
+        return (
+          <S.NavItem key={idx}>
+            {renderNavLink(`/${item.link}`, item.name)}
+          </S.NavItem>
+        );
+      }
+
+      return (
+        <S.NavItem
+          key={idx}
+          onMouseEnter={() => setHoveredItem(idx)}
+          onMouseLeave={() => setHoveredItem(null)}
+        >
+          <Link to="#">
+            <Text color="White" typograph="lg_regular">
+              {item.name}
+            </Text>
+          </Link>
+          {isHovered && (
+            <S.NestedItemsContainer>
+              {item.nested.map((nested) => (
+                <S.NestedItem key={nested.name}>
+                  {nested.link === '#' ? (
+                    <Text color="White" typograph="lg_regular">
+                      {nested.name}
+                    </Text>
+                  ) : (
+                    renderNavLink(`/${nested.link}`, nested.name)
+                  )}
+                </S.NestedItem>
+              ))}
+            </S.NestedItemsContainer>
+          )}
+        </S.NavItem>
+      );
+    });
+
+  const renderAuthItem = () => {
+    return accessToken === null || accessToken === '' ? (
+      <S.NavItem key="login">{renderNavLink('/login', '로그인')}</S.NavItem>
+    ) : (
+      <S.NavItem key="logout" onClick={() => setIsModalOpen(true)}>
+        <Text
+          color="White"
+          typograph="lg_regular"
+          style={{ cursor: 'pointer' }}
+        >
+          로그아웃
+        </Text>
+      </S.NavItem>
+    );
+  };
+  return (
+    <>
+      <S.NavigationContainer className="layout-padding">
+        <S.LogoContainer>
+          <Link to="/">
+            <img src={LogoWhiteSVG} alt="logo" style={{ width: '100%' }} />
+          </Link>
+        </S.LogoContainer>
+
+        <S.NavItemsContainer>
+          {renderNavItems()}
+          {renderAuthItem()}
+        </S.NavItemsContainer>
+
+        <S.MobileMenuButton
+          onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+        >
+          <span />
+          <span />
+          <span />
+        </S.MobileMenuButton>
+
+        {isModalOpen && (
+          <Modal
+            isOpen={isModalOpen}
+            onLogout={() => {
+              logoutMutation.mutate();
+              setIsModalOpen(false);
+            }}
+            onClose={() => setIsModalOpen(false)}
+          ></Modal>
+        )}
+
+        <MobileNavigation
+          navList={navList}
+          isMobileMenuOpen={isMobileMenuOpen}
+          setIsMobileMenuOpen={setIsMobileMenuOpen}
+        />
+      </S.NavigationContainer>
+    </>
+  );
+};
+
+export default Navigation;
---
+++ src/components/common/Navigation/style.ts
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled';
+import { BREAKPOINT } from '../../../lib/constants';
+
+export const S = {
+  NavigationContainer: styled.nav`
+    position: absolute;
+    top: 0px;
+    width: 100%;
+    height: 70px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    user-select: none;
+  `,
+  LogoContainer: styled.div`
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    width: 150px;
+    @media (max-width: ${BREAKPOINT.tablet}) {
+      width: 100px;
+    }
+  `,
+  NavItemsContainer: styled.ul`
+    display: flex;
+    line-height: 70px;
+    list-style-type: none;
+
+    @media (max-width: ${BREAKPOINT.tablet}) {
+      display: none;
+    }
+  `,
+  NavItem: styled.li`
+    margin: 0px 16px;
+    text-decoration-line: none;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    a {
+      text-decoration: none;
+    }
+  `,
+  NestedItemsContainer: styled.ul`
+    padding: 5px 50px;
+    position: absolute;
+
+    display: flex;
+    gap: 20px;
+    width: max-content;
+    height: 50px;
+
+    transform: translate(-50%, 0px);
+    margin-left: 50%;
+    list-style-type: none;
+
+    border-radius: 30px;
+    background-color: #151515;
+  `,
+  NestedItem: styled.li`
+    line-height: 1;
+    display: flex;
+    align-items: center;
+  `,
+  MobileMenuButton: styled.div`
+    display: none;
+    flex-direction: column;
+    cursor: pointer;
+    span {
+      width: 25px;
+      height: 3px;
+      background-color: #ffffff;
+      margin: 3px 0;
+      border-radius: 5px;
+    }
+
+    @media (max-width: 767px) {
+      display: flex;
+    }
+  `,
+};
