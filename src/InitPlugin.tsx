313d67c4561d1d0a3fd6052ba3a7d55f54e89595
최동준 <coine00.choi@gmail.com>
2025-04-18 16:02:17 +0900

    fix: react strict 모드로 인한 refresh 이슈 해결

--- src/InitPlugin.tsx
+++ src/InitPlugin.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRefreshTokenMutation } from './apis';
 
 const InitPlugin = ({ children }: { children: React.ReactNode }) => {
   const [isReady, setIsReady] = useState(false);
   const refreshMutation = useRefreshTokenMutation();
 
-  // 최초 접속 혹은 새로고침 시 토큰 처리
+  const hasRefreshed = useRef(false);
+
   useEffect(() => {
+    if (hasRefreshed.current) return;
+    hasRefreshed.current = true;
+
     refreshMutation.mutateAsync().finally(() => {
       setIsReady(true);
     });
