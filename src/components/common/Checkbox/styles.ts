cc1061b340fa3b1d34cfc8be504c59c2c894515c
최동준 <coine00.choi@gmail.com>
2025-04-18 13:48:58 +0900

    feat: 공통 checkbox 컴포넌트 개발

---
+++ src/components/common/Checkbox/styles.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+export const RadioContainer = styled.label`
+  display: flex;
+  position: relative;
+`;
+
+export const RadioInput = styled.input`
+  position: absolute;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+  margin: 0px;
+  padding: 0px;
+  z-index: 1;
+  &:checked + span .unchecked {
+    display: none;
+  }
+  &:not(:checked) + span .checked {
+    display: none;
+  }
+`;
+
+export const RadioButton = styled.span`
+  position: relative;
+  display: flex;
+  width: 24px;
+  height: 24px;
+  & > img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+  &.large {
+    width: 44px;
+    height: 44px;
+  }
+  &.small {
+    width: 18px;
+    height: 18px;
+  }
+`;
+
+export const RadioLabel = styled.div`
+  margin-left: 27px;
+  color: inherit;
+  font-size: 2rem;
+`;
